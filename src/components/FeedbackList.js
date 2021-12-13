cb9936636477c8d1849f91c1d016e15483c9a58e
Tanner Gates <tgates4@dmail.dixie.edu>
2021-12-13 08:20:08 -0700

    Add Context to app

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useContext } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import FeedbackItem from './FeedbackItem'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackList({ feedback, handleDelete }) {
+function FeedbackList() {
+  const { feedback } = useContext(FeedbackContext)
   if (!feedback || feedback.length === 0) {
     return <p>No Feedback Yet</p>
   }
@@ -13,7 +14,7 @@ function FeedbackList({ feedback, handleDelete }) {
       <AnimatePresence>
         {feedback.map(item => (
           <motion.div key={item.id} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-            <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+            <FeedbackItem key={item.id} item={item} />
           </motion.div>
         ))}
       </AnimatePresence>
@@ -30,14 +31,14 @@ function FeedbackList({ feedback, handleDelete }) {
   // )
 }
 
-FeedbackList.propTypes = {
-  feedback: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      text: PropTypes.string.isRequired,
-      rating: PropTypes.number.isRequired
-    })
-  )
-}
+// FeedbackList.propTypes = {
+//   feedback: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//       text: PropTypes.string.isRequired,
+//       rating: PropTypes.number.isRequired
+//     })
+//   )
+// }
 
 export default FeedbackList
--- src/components/FeedbackStats.js
+++ src/components/FeedbackStats.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useContext } from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackStats({ feedback }) {
+function FeedbackStats() {
+  const { feedback } = useContext(FeedbackContext)
   //calculate ratings average
 
   let average =
@@ -12,15 +13,15 @@ function FeedbackStats({ feedback }) {
   average = average.toFixed(1).replace(/[.,]0$/, '')
 
   return (
-    <div className="feedback-stats">
+    <div className='feedback-stats'>
       <h4>{feedback.length} Reviews</h4>
       <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
     </div>
   )
 }
 
-FeedbackStats.propTypes = {
-  feedback: PropTypes.array.isRequired
-}
+// FeedbackStats.propTypes = {
+//   feedback: PropTypes.array.isRequired
+// }
 
 export default FeedbackStats
