0b2009fda33b8fc2862ed6a75a1bd370aae4cd6b
Tanner Gates <tgates4@dmail.dixie.edu>
2021-12-14 10:37:11 -0700

    Add button to edit feedback item

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -22,6 +22,11 @@ export const FeedbackProvider = ({ children }) => {
     }
   ])
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false
+  })
+
   const deleteFeedback = id => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter(item => item.id !== id))
@@ -33,12 +38,20 @@ export const FeedbackProvider = ({ children }) => {
     setFeedback([newFeedback, ...feedback])
   }
 
+  const editFeedback = item => {
+    setFeedbackEdit({
+      item,
+      edit: true
+    })
+  }
+
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
         deleteFeedback,
-        addFeedback
+        addFeedback,
+        editFeedback
       }}
     >
       {children}
