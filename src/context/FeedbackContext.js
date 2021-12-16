78685b943ed10c834f6341eb487345b43bae48c0
Tanner Gates <tgates4@dmail.dixie.edu>
2021-12-16 06:54:14 -0700

    Create mock backend json-server and add get request to contest

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,29 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This is feedback item 1',
-      rating: 10
-    },
-    {
-      id: 2,
-      text: 'This is feedback item 2',
-      rating: 10
-    },
-    {
-      id: 3,
-      text: 'This is feedback item 3',
-      rating: 10
-    }
-  ])
-
+  const [feedback, setFeedback] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false
   })
+
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+
+  // Fetch feedback
+
+  const fetchFeedback = async () => {
+    const response = await fetch('http://localhost:5000/feedback?_sort=id&_order=desc')
+    const data = await response.json()
+
+    setFeedback(data)
+    setIsLoading(false)
+  }
 
   const updateFeedback = (id, updItem) => {
     setFeedback(feedback.map(item => (item.id === id ? { ...item, ...updItem } : item)))
@@ -57,7 +55,8 @@ export const FeedbackProvider = ({ children }) => {
         addFeedback,
         editFeedback,
         feedbackEdit,
-        updateFeedback
+        updateFeedback,
+        isLoading
       }}
     >
       {children}
